9183a08a1f19cda1aef1b79518269a7b44fb6258
Mac Starkebaum <mstarkeb@uoguelph.ca>
2023-01-04 20:46:00 -0500

    inital commit

---
+++ script.js
@@ -0,0 +1,93 @@
+window.addEventListener("load", () => {
+  nameAnimation();
+  addDescObservers();
+  addTimelineObservcers();
+  laptopFlicker();
+  returnToTop();
+});
+
+const nameAnimation = () => {
+  const arr = document.getElementsByClassName("hovertext");
+  const addClass = (index) => {
+    if (index < arr.length) {
+      if (index === 6) {
+        removeClass(0);
+      }
+      arr[index].classList.add("hovering");
+      setTimeout(() => {
+        addClass(index + 1);
+      }, 120);
+    }
+  };
+
+  const removeClass = (index) => {
+    if (index < arr.length) {
+      arr[index].classList.remove("hovering");
+      setTimeout(() => {
+        removeClass(index + 1);
+      }, 150);
+    }
+  };
+
+  addClass(0);
+};
+
+const addDescObservers = () => {
+  const elemsObj = document.getElementsByClassName("description");
+
+  let observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("description-bordered");
+        } else {
+          entry.target.classList.remove("description-bordered");
+        }
+      });
+    },
+    { threshold: 0.2, rootMargin: "-35% 0% -35% 0%" }
+  );
+  Object.keys(elemsObj).forEach((key) => {
+    observer.observe(elemsObj[key]);
+  });
+};
+
+const addTimelineObservcers = () => {
+  const elemsObj = document.getElementsByClassName("timeline-step");
+
+  let observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("current-timeline-step");
+        } else {
+          entry.target.classList.remove("current-timeline-step");
+        }
+      });
+    },
+    { threshold: 0, rootMargin: "-40% 0% -40% 0%" }
+  );
+  Object.keys(elemsObj).forEach((key) => {
+    observer.observe(elemsObj[key]);
+  });
+};
+
+const laptopFlicker = () => {
+  const elemsObj = document.querySelector("nav h1 [class*='fa-'] ");
+
+  elemsObj.addEventListener("click", () => {
+    if (elemsObj.classList.contains("header-alt-color")) {
+      elemsObj.classList.remove("header-alt-color");
+    } else {
+      elemsObj.classList.add("header-alt-color");
+    }
+  });
+};
+
+const returnToTop = () => {
+  const elem = document.getElementById("return-to-top");
+  elem.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  });
+};
